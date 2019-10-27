0955eed058c10b19cd85e6b9a754ba82725e0e07
Ramtin Khalatbari <versayana@outlook.com>
2019-10-27 13:42:22 +0330

    Use event binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,24 +4,11 @@ import { CoursesService } from './courses.service';
 @Component({
   selector: 'courses',
   template: `
-  <button class="btn btn-primary" [class.active]="isActive">Save</button>
-  <button [style.backgroundColor]="isActive ? 'blue' : 'white'">Save</button>
-  <!-- <h2>
-    {{ title }}
-  </h2>
-  <ul>
-    <li *ngFor="let course of courses">
-      {{ course }}
-    </li>
-  </ul> -->
+  <button (click)="saveClicked($event)">Save</button>
   `
 })
 export class CoursesComponent {
-  isActive = true;
-  title = "List of courses";
-  courses: string[];
-
-  constructor(service: CoursesService) {
-    this.courses = service.getCourses();
+  saveClicked($event) {
+    console.log("Save button was clicked", $event)
   }
 }
