a0433870d2ee921a60011b303262497d3260d5cb
Ramtin Khalatbari <versayana@outlook.com>
2019-10-27 14:26:24 +0330

    Use custom pipe

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,9 +4,9 @@ import { CoursesService } from './courses.service';
 @Component({
   selector: 'courses',
   template: `
-  <p>{{ releaseDate | date: shortDate }}</p>
+  <p>{{ longText | summary: 25 }}</p>
   `
 })
 export class CoursesComponent {
-  releaseDate = new Date();
+  longText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 }
