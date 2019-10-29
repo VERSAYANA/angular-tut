5d0e86003f093e7c29787cf6397d8c600b18a7b0
Ramtin Khalatbari <versayana@outlook.com>
2019-10-29 16:44:09 +0330

    Use costum directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FavoriteComponent } from './favorite/favorite.component';
 import { TitlePipe } from './title.pipe';
 import { PanelComponent } from './panel/panel.component';
 import { LikeComponent } from './like/like.component';
+import { InputFormatDirective } from './input-format.directive';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { LikeComponent } from './like/like.component';
     FavoriteComponent,
     TitlePipe,
     PanelComponent,
-    LikeComponent
+    LikeComponent,
+    InputFormatDirective
   ],
   imports: [
     BrowserModule,
---
+++ src/app/input-format.directive.ts
@@ -0,0 +1,18 @@
+import { Directive, Input, ElementRef, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appInputFormat]'
+})
+export class InputFormatDirective {
+  @Input('appInputFormat') format: string;
+  constructor(private el: ElementRef) {}
+
+  @HostListener('blur') onBlur() {
+    const value: string = this.el.nativeElement.value;
+    if (this.format === 'uppercase') {
+      this.el.nativeElement.value = value.toUpperCase();
+    } else {
+      this.el.nativeElement.value = value.toLowerCase();
+    }
+  }
+}
